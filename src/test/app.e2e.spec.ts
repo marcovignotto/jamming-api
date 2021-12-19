a39a54c2830947424efdc256b567aa9a8627a314
Marco Vignotto <writeme@marcovignotto.com>
2021-12-19 17:20:24 +0100

    test players ok

--- src/test/app.e2e.spec.ts
+++ src/test/app.e2e.spec.ts
@@ -8,6 +8,12 @@ import { AppModule } from '../app.module';
 import {
   credentialUserGeneral,
   credentialUserAdmin,
+  credentialBobDylan,
+  credentialJohnColtrane,
+  credentialJoniMitchell,
+  credentialNeilYoung,
+  credentialYokoOno,
+  tokenBodDylan,
 } from './utils/testCredentials';
 
 import apiVersion from '../../config/apiVersion';
@@ -21,9 +27,11 @@ const objPostUser = credentialUserGeneral();
 const objUpdateUser = credentialUserAdmin();
 
 // paths
+const PATH_USERS = API_VERSION + '/users';
 const PATH_AUTH = API_VERSION + '/auth';
+const PATH_JAMS = API_VERSION + '/jams';
 
-describe.skip('AppController (e2e)', () => {
+describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -47,7 +55,7 @@ describe.skip('AppController (e2e)', () => {
     // test the empty user db
     it(`GET > [] `, async () => {
       return await request(app.getHttpServer())
-        .get('/users')
+        .get(PATH_USERS)
         .expect(200)
         .expect([]);
     });
@@ -56,7 +64,7 @@ describe.skip('AppController (e2e)', () => {
     it('POST > 201 and user First name Last name and Email ', async () => {
       // request into var
       const req = await request(app.getHttpServer())
-        .post('/users')
+        .post(PATH_USERS)
         .send(objPostUser)
         .expect(201)
         .then((res) => JSON.parse(res.text));
@@ -73,7 +81,7 @@ describe.skip('AppController (e2e)', () => {
 
     it('PUT > 200 and user updated data', async () => {
       const req = await request(app.getHttpServer())
-        .put(`/users/${userIdToUpdate}`)
+        .put(PATH_USERS + `/${userIdToUpdate}`)
         .send(objUpdateUser)
         .expect(200)
         .then((res) => JSON.parse(res.text));
@@ -89,7 +97,7 @@ describe.skip('AppController (e2e)', () => {
 
     it('GET - New all users db > length 1', async () => {
       const req = await request(app.getHttpServer())
-        .get('/users')
+        .get(PATH_USERS)
         .expect(200)
         .then((res) => JSON.parse(res.text));
 
@@ -153,6 +161,62 @@ describe.skip('AppController (e2e)', () => {
       const req = await request(app.getHttpServer())
         .get(PATH_AUTH)
         .set('Authorization', 'Bearer ' + token)
+        .expect(200)
+        .then((res) => res.body);
+
+      expect.assertions(3);
+
+      // use returned values
+      expect(req.firstName).toBe(objPostUser['firstName']);
+      expect(req.lastName).toBe(objPostUser['lastName']);
+      expect(req.email).toBe(objPostUser['email']);
+    });
+  });
+
+  // to create a jam we need a few users (players) with diffrerent instruments
+  // one creates the jam for 4 players  in total and other 3 join
+  // one arrives late and can't join
+
+  describe.skip('Create users for the /jams test', () => {
+    // players to create into array
+    const arrayOfPlayers = [
+      credentialBobDylan(),
+      credentialJohnColtrane(),
+      credentialJoniMitchell(),
+      credentialNeilYoung(),
+      credentialYokoOno(),
+    ];
+
+    // loop to create
+    for (let i = 0; i < arrayOfPlayers.length; i++) {
+      const player = arrayOfPlayers[i];
+
+      it(`Create player ${player['firstName']} ${player['lastName']} with ${player['instrument']} `, async () => {
+        const req = await request(app.getHttpServer())
+          .post(API_VERSION + '/users')
+          .send(player)
+          .expect(201)
+          .then((res) => JSON.parse(res.text));
+
+        expect.assertions(3);
+
+        // use returned values
+        expect(req['firstName']).toBe(player['firstName']);
+        expect(req['lastName']).toBe(player['lastName']);
+        expect(req['email']).toBe(player['email']);
+      });
+    }
+  });
+
+  // create the jam
+  describe.skip('/jams - GET - POST - PUT - DELETE', () => {
+    // to have a simpler authorization some token
+    const tokenJamHost = tokenBodDylan();
+
+    it('GET > 200 and all the jams [] length 0 ', async () => {
+      const req = await request(app.getHttpServer())
+        .get(PATH_JAMS)
+        .set('Authorization', 'Bearer ' + tokenJamHost)
         .expect(200)
         .then((res) => res.body);
 
--- src/test/utils/testCredentials.ts
+++ src/test/utils/testCredentials.ts
@@ -29,3 +29,79 @@ export const credentialUserAdmin = (): object => {
 export const credentialUserGeneral = (): object => {
   return userGeneral;
 };
+
+// bob dylan
+const userBobDylan = {
+  firstName: 'Bod',
+  lastName: 'Dylan',
+  email: 'bobdylan@gmail.com',
+  password: '12345678',
+  instrument: 'Harmonica',
+  role: 'user',
+};
+
+// token with bob Dylan credentials
+// to simplify authorization
+
+export const tokenBodDylan = () =>
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImJvYmR5bGFuQGdtYWlsLmNvbSIsImlhdCI6MTYzOTkzMDQ1MSwiZXhwIjoxNjQ1MTE0NDUxfQ.QuxidLg9mGLTmOR4s-Zpfq6Dubt_Hee9u782fFYGrMQ';
+
+export const credentialBobDylan = (): object => {
+  return userBobDylan;
+};
+
+// Neil Young
+const userNeilYoung = {
+  firstName: 'Neil',
+  lastName: 'Young',
+  email: 'neilyoung@gmail.com',
+  password: '12345678',
+  instrument: 'Guitar',
+  role: 'user',
+};
+
+export const credentialNeilYoung = (): object => {
+  return userNeilYoung;
+};
+
+// Joni Mitchell
+const userJoniMitchell = {
+  firstName: 'Joni',
+  lastName: 'Mitchell',
+  email: 'jonimitchel@gmail.com',
+  password: '12345678',
+  instrument: 'Voice',
+  role: 'user',
+};
+
+export const credentialJoniMitchell = (): object => {
+  return userJoniMitchell;
+};
+
+// John Coltrane
+const userJohnColtrane = {
+  firstName: 'John',
+  lastName: 'Coltrane',
+  email: 'johncoltrane@gmail.com',
+  password: '12345678',
+  instrument: 'Sax',
+  role: 'user',
+};
+
+export const credentialJohnColtrane = (): object => {
+  return userJohnColtrane;
+};
+
+// Yoko Ono
+const userYokoOno = {
+  firstName: 'Yoko',
+  lastName: 'Ono',
+  email: 'yokoono@gmail.com',
+  password: '12345678',
+  instrument: 'Voice',
+  role: 'user',
+};
+
+export const credentialYokoOno = (): object => {
+  return userYokoOno;
+};
